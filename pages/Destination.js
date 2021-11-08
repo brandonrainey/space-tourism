aeb157d4e75b9d25e9ae36852240a39789bf16ad
Brandon <noneya67@yahoo.com>
2021-11-08 02:30:37 -0500

    templated the destination page

--- pages/Destination.js
+++ pages/Destination.js
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from '../components/Navbar'
+import data from '../data.json'
+import img from '../assets/destination/image-moon.png'
 
 export default function Destination() {
+
+    useEffect(() => {
+        console.log(data.destinations[0].name)
+    },[])
     return (
-        <div className='destinationBackground flex'>
+        <div className='destinationBackground flex flex-col'>
             <Navbar />
+            <div className='destinationTitle'><span className='destinationTitleNum'>01</span> PICK YOUR DESTINATION</div>
+            <div className='flex'>
+                
+                <div className='planetImg'>
+                    <img src={'../img/image-moon.png'}/>
+                </div>
+                <div className='destinationContainer'>
+                    <div className='destinationNav'>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className='planetName'>
+
+                    </div>
+                    <div className='planetDescription'>
+
+                    </div>
+                    <div className='destinationLine'>
+
+                    </div>
+                    <div className='footerInfo'>
+                        <div className='planetDistance'>
+
+                        </div>
+                        <div className='travelTime'>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,9 @@ import React from "react"
 import Navbar from "../components/Navbar"
 
 
+
 export default function Home() {
-  
+  const destinations = []
 
   return (
     <div className='homeBackground flex'>
